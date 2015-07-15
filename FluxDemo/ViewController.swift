1a6f6edbdab582fd8b4a6d6c128ff29be33ff60b
Ruben Roques <rubenroques.nhk@gmail.com>
2015-07-15 23:52:00 +0100

    Initial Commit

---
+++ FluxDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FluxDemo
+//
+//  Created by Ruben Roques on 15/07/15.
+//  Copyright (c) 2015 RRocks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
